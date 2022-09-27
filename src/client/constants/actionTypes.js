29d199025f2c476930e5ba1a4dea1502246db0a3
Hannah McDowell <hannah.mcdowell1@gmail.com>
2022-09-27 14:25:28 -0700

    Add types to actionTypes.

---
+++ src/client/constants/actionTypes.js
@@ -0,0 +1,7 @@
+export const LOG_IN = 'LOG_IN';
+export const SIGN_UP = 'SIGN_UP';
+export const LOG_OUT = 'LOG_OUT';
+export const CREATE_MED = 'CREATE_MED';
+export const UPDATE_MED = 'UPDATE_MED';
+export const DELETE_MED = 'DELETE_MED';
+
