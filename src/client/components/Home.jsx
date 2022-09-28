284e1da242bdda231a651a96068353bb0476a16b
Hannah McDowell <hannah.mcdowell1@gmail.com>
2022-09-27 22:52:40 -0700

    Add import statments.

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import Calendar from './Calendar';
+import PrescriptionContainer from './PrescriptionContainer';
 
 const Home = () => {
 
   return (
     <section className="home">
-       <Calendar />
-       <PrescriptionContainer />
+      <Calendar />
+      <PrescriptionContainer />
     </section>
   );
 };
