8942db54245db68d6ace7226a4437cbba85d813c
Hannah McDowell <hannah.mcdowell1@gmail.com>
2022-09-28 10:40:33 -0700

    Create grid structure for homepage.

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,15 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Unstable_Grid2';
 import Calendar from './Calendar';
 import PrescriptionContainer from './PrescriptionContainer';
 
 const Home = () => {
 
   return (
-    <section className="home">
-      <Calendar />
-      <PrescriptionContainer />
-    </section>
+    <Container maxWidth="lg">
+      <Grid container spacing={{ xs: 2, md: 3 }}>
+        <Grid xs={12} md={6}>
+          <Calendar />
+        </Grid>
+        <Grid xs={12} md={6}>
+          <PrescriptionContainer />
+        </Grid>
+      </Grid>
+    </Container>
+    // <section className="home">
+    //   
+    //   <PrescriptionContainer />
+    // </section>
   );
 };
 
